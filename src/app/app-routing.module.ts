7d82c1feaf0db97e3416c5bfd63883951ded8c5e
M4K4TT4CK <mikemcmillin@gmail.com>
2023-08-06 15:58:23 -0400

    Final default url adjustment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { WorldMapComponent } from './components/worldMap/worldMap.component';
 
 const routes: Routes = [
-    { path: '', component: WorldMapComponent},
-    { path: '', redirectTo: './worldMap', pathMatch: 'full' }
+    { path: '', component: WorldMapComponent}
 ];
 
 @NgModule({
